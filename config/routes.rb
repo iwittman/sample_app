b49057eeb767e055118f359500f545f2b714b5f4
Isaac Wittman <isaacwittman@gmail.com>
2014-08-18 22:12:50 -0400

    debug adding microposte

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ SampleApp::Application.routes.draw do
   match '/signin', to: 'sessions#new', via: 'get'
   match '/signout', to: 'sessions#destroy', via: 'delete'
 
+  resources :microposts, only: [:create, :destroy]
+
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
